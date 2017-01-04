75d0733c8375fa378efd427c71a598e2591a254d
roansong <roansong@gmail.com>
2017-01-04 18:13:27 +0200

    gitignore stuff

--- nearest_neighbour/nearest_neighbour.py
+++ nearest_neighbour/nearest_neighbour.py
@@ -33,6 +33,37 @@ def pad_img(img,IMG_HEIGHT,IMG_WIDTH,IN_HEIGHT,IN_WIDTH):
     npad = ((tpad,bpad),(lpad,rpad))
     img = np.pad(img, pad_width=npad, mode='constant',constant_values=0)
 
+
+    return img
+
+def crop_img(img,IMG_HEIGHT,IMG_WIDTH,IN_HEIGHT,IN_WIDTH):
+    
+    vpad = (IN_HEIGHT - IMG_HEIGHT)/2
+    if not (np.floor(vpad) ==  vpad):
+        tpad = np.floor(vpad)
+        bpad = (vpad + 1)
+    else:
+        tpad = vpad
+        bpad = vpad
+    
+    hpad = (IN_WIDTH - IMG_WIDTH)/2
+    if not (np.floor(hpad) == hpad):
+        lpad = np.floor(hpad)
+        rpad = (hpad + 1)
+    else:
+        lpad = hpad
+        rpad = hpad
+        
+    tpad = (int)(tpad)
+    bpad = (int)(bpad)
+    lpad = (int)(lpad)
+    rpad = (int)(rpad)
+    
+    npad = ((tpad,bpad),(lpad,rpad))
+    
+    img = img[lpad: (IN_WIDTH - rpad), tpad: (IN_HEIGHT - bpad)]
+    
+
     return img
 
 best_dist = np.inf;
@@ -46,8 +77,8 @@ dt = np.dtype([('filename','|S20'),('labels',np.int32,(num_classes,))])
 filedata = np.loadtxt(location + 'filenames.txt',dtype=dt)
 
 
-IMG_HEIGHT = 178 # biggest picture in the set
-IMG_WIDTH = 177
+IMG_HEIGHT = 40 # biggest picture in the set
+IMG_WIDTH = 40
 
 
 
@@ -63,11 +94,15 @@ for n in range(filedata['filename'].shape[0]):
     infile = (filedata['filename'][n]).decode('UTF-8')
     correct_output = (filedata['labels'][n])
    
-    
+    print(infile)
     in_img = mpimg.imread(location +infile)
     IN_HEIGHT = in_img.shape[0]
     IN_WIDTH = in_img.shape[1]
-    in_img = pad_img(in_img,IMG_HEIGHT,IMG_WIDTH,IN_HEIGHT,IN_WIDTH)
+
+    if(IN_HEIGHT < IMG_HEIGHT):
+        in_img = pad_img(in_img,IMG_HEIGHT,IMG_WIDTH,IN_HEIGHT,IN_WIDTH)
+    else:
+        in_img = crop_img(in_img,IMG_HEIGHT,IMG_WIDTH,IN_HEIGHT,IN_WIDTH)
     in_oneD = in_img.reshape(IMG_HEIGHT * IMG_WIDTH)
     
     for k in range(filedata['filename'].shape[0]):
@@ -83,7 +118,15 @@ for n in range(filedata['filename'].shape[0]):
     
         CUR_HEIGHT = cur_img.shape[0]
         CUR_WIDTH = cur_img.shape[1]
-        cur_img = pad_img(cur_img,IMG_HEIGHT,IMG_WIDTH,CUR_HEIGHT,CUR_WIDTH)
+        
+        if(CUR_HEIGHT < IMG_HEIGHT):
+            cur_img = pad_img(cur_img,IMG_HEIGHT,IMG_WIDTH,CUR_HEIGHT,CUR_WIDTH)
+        else:
+            cur_img = crop_img(cur_img,IMG_HEIGHT,IMG_WIDTH,CUR_HEIGHT,CUR_WIDTH)
+        
+        
+        
+        
         
         cur_oneD = cur_img.reshape(IMG_HEIGHT * IMG_WIDTH)
     
